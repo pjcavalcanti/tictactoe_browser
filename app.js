c925c670ca078284a4c9d78f0c5f8789f451cc19
Paulo Cavalcanti <paulojcvf@gmail.com>
2024-08-16 00:07:12 +0400

    minimum working game completed

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 let turnPointer = 0;
+let points_cross = document.querySelector("#p2 .points");
+let points_circle = document.querySelector("#p1 .points");
+let points = [points_cross, points_circle];
 
 const cross = document.querySelector(".shapes .cross").outerHTML;
 const circle = document.querySelector(".shapes .circle").outerHTML;
@@ -6,38 +9,84 @@ const turnShapes = [cross, circle];
 
 const squares = document.querySelectorAll(".square");
 
-console.dir(squares);
-
 function highlightSquare(event) {
   event.target.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
 }
 function deHighlightSquare(event) {
   event.target.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
 }
+function checkWin(square) {
+  let mark;
+  if (turnPointer == 0) {
+    mark = "cross";
+  } else {
+    mark = "circle";
+  }
 
-function toggleTurn() {
-  turnPointer = (turnPointer + 1) % 2;
-  console.log(turnPointer);
+  let i = +square.id.slice(1)[0];
+  let j = +square.id.slice(1)[1];
+
+  let won = true;
+  for (let col = 0; col < 3; col++) {
+    if (!squares[col + i * 3].innerHTML.includes(mark)) {
+      won = false;
+    }
+  }
+  if (won) {
+    return won;
+  }
+  won = true;
+  for (let row = 0; row < 3; row++) {
+    if (!squares[j + row * 3].innerHTML.includes(mark)) {
+      won = false;
+    }
+  }
+  if (won) {
+    return won;
+  }
+
+  won = true;
+  for (let diag = 0; diag < 3; diag++) {
+    if (!squares[diag + diag * 3].innerHTML.includes(mark)) {
+      won = false;
+      break;
+    }
+  }
+  if (won) {
+    return won;
+  }
+
+  won = true;
+  for (let offset = 0; offset < 3; offset++) {
+    if (!squares[(offset) + (2 - offset) * 3].innerHTML.includes(mark)) {
+      won = false;
+      break;
+    }
+  }
+  return won;
 }
-function checkWinner(coord) {
-  return coord[0] == 0;
-}
+
 function makePlay(event) {
   let square = event.target;
   if (square.children.length > 0) {
     return;
   }
   square.innerHTML = turnShapes[turnPointer]; // put shape in place
+  console.log("i win: ", checkWin(square));
+  if (checkWin(square)) {
+    points[turnPointer].innerHTML = 1 + parseInt(points[turnPointer].innerHTML, 10);
+    for (let square of squares) {
+      square.innerHTML = "";
+    }
+  }
   turnPointer = (turnPointer + 1) % 2; // change player turns
-  console.log(checkWinner(square.id.slice(1))); // check if the player won
 }
 
-for (square of squares) {
+for (let square of squares) {
   if (square.classList.length != 1) {
     continue;
   }
   square.addEventListener("mouseenter", highlightSquare);
   square.addEventListener("mouseleave", deHighlightSquare);
-  square.addEventListener("click", putShape);
-  console.log(square)
+  square.addEventListener("click", makePlay);
 }
